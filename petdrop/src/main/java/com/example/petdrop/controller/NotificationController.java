6ebfbd987b3e1d60801620412676baf6e68e7e16
blakebryan <bbryan0316@gmail.com>
2025-10-03 15:52:30 -0500

    did some work on notification endpoints - more to be done later

--- petdrop/src/main/java/com/example/petdrop/controller/NotificationController.java
+++ petdrop/src/main/java/com/example/petdrop/controller/NotificationController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,11 +18,18 @@ public class NotificationController {
     @Autowired
     private NotificationRepository repo;
 
+    // TODO
     @PostMapping("/add-notification")
     public DatabaseNotification addNotification(@RequestBody NotificationRequest notificationRequest) {
         return repo.save(NotificationRequest.makeIntoDBNotif(notificationRequest));
     }
 
+    @PutMapping("update-notification")
+    public DatabaseNotification updateNotification(@RequestBody NotificationRequest notificationRequest) {
+        return repo.save(NotificationRequest.makeIntoDBNotif(notificationRequest));
+    }
+
+    // TODO
     @DeleteMapping("/delete-notification/{id}")
     public void deleteNotification(@PathVariable String id) {
         repo.findById(id).ifPresent(n -> {
